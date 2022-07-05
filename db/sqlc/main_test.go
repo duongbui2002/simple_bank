d53729ca8af57d05913b17c0e066baf96ed64c0f
duongbui2002 <duongbuidinh593@gmail.com>
2022-07-05 19:38:19 +0700

    init git

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
