91efd272fad65e8f1c925c7234381abe3a2031f6
duongbui2002 <duongbuidinh593@gmail.com>
2022-07-06 19:53:40 +0700

    hash password

--- Makefile
+++ Makefile
@@ -6,12 +6,20 @@ dropdb:
 	docker exec -it postgres12 dropdb  simple_bank
 migrateup:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable" -verbose up
+migrateup1:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable" -verbose up 1
 migratedown:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable" -verbose down
+
+migratedown1:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable" -verbose down 1
 sqlc: 
 	docker run --rm -v ${pwd}:/src -w /src kjconroy/sqlc generate
 test:
 	go test -v -cover ./...
 server:
 	go run main.go
-.PHONY: postgres12 createdb dropdb migrateup migratedown sqlc test server
+mock:
+	 mockgen -package mockdb -destination db/mock/store.go github.com/golandprojects/simplebank/db/sqlc Store
+
+.PHONY: postgres12 createdb dropdb  migrateup migratedown migrateup1 migratedown1 sqlc test server
--- api/account.go
+++ api/account.go
@@ -4,17 +4,18 @@ import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
 	db "github.com/golandprojects/simplebank/db/sqlc"
+	"github.com/lib/pq"
 	"net/http"
 )
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR" `
+	Currency string `json:"currency" binding:"required,currency" `
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
-	if err := ctx.BindJSON(&req); err != nil {
+	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
@@ -26,6 +27,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
